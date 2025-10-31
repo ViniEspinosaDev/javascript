23132f4b4c5396f1bfccc5a095474b3d2e1eb9f6
ViniEspinosaDev <viniespinosa.developer@gmail.com>
2025-10-31 08:27:45 -0300

    arithmetic operator

--- scripts.js
+++ scripts.js
@@ -1,25 +1,6 @@
-/* 
-    Conversão de tipos
-    - feito de forma consciente
-*/
-
-console.log(typeof 9); // number
-console.log(typeof "9"); // string
-console.log(typeof Number("9")); // number
-
-let age = 18;
-console.log(typeof age.toString()); // string
-console.log(typeof String(age)) // string
-
-let option = 1
-console.log(option)
-console.log(Boolean(option))
-console.log(typeof option)
-console.log(typeof Boolean(option))
-
-/* 
-    Coersão de tipo 
-    - acontece de forma automática (implicitamente)
-*/
-console.log("10" + 5) // 105
-console.log(typeof ("10" + 5)) // string
+console.log("SOMA: ", 5 + 3 + 8)
+console.log("CONCATENAÇÃO: ", 5 + "5")
+console.log("DIVISÃO: ", 10 / 3)
+console.log("MULTIPLICAÇÃO: ", 10 * 3)
+console.log("RESTO DA DIVISÃO: ", 10 % 3)
+console.log("EXPONENCIAL: ", 2 ** 3)
