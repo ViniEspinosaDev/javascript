5e1c265828469240a537d4a73c58b0e54bcb13b6
ViniEspinosaDev <viniespinosa.developer@gmail.com>
2025-10-31 07:31:45 -0300

    Variables

--- scripts.js
+++ scripts.js
@@ -1,6 +1,14 @@
-// Comentários
-console.log("Hello World!")
+// Declara uma variável sem valor
+var user 
+console.log(user)
 
-/* Exemplos de comentários 
-    de multiplas linhas
-*/
+// Declara variável e atribui valor
+var email = "vini@email.com"
+console.log(email)
+
+// Substituir valor da variável
+email = "joao@email.com"
+console.log(email)
+
+email = 23
+console.log(email)
