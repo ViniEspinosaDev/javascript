412e6f69b2506b3ff3e91e891f9e62f11bfe4b6c
ViniEspinosaDev <viniespinosa.developer@gmail.com>
2025-10-31 08:40:08 -0300

    equal diff strictly eq or diff

--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
-// Ordem de precedência - prioridade dos operadores (**; *,/,//,%; +,-; ==, !=, <=, >=, >, <; not; and; or)
-let equation = 2 + 3 * 4 // 3 * 4 = 12 + 2 == 14
-console.log(equation) // 14
-equation = (2 + 3) * 4 // (2 + 3) = 5 * 4 == 20
-console.log(equation) // 20
+let one = 1
+let two = 2
+
+console.log(one == two) // false
+console.log(one == 1) // true
+console.log(one == "1") // true (olha para o conteúdo e não para o tipo)
+
+// Estritamente igual (tipo e valor)
+console.log(one === "1") // false (olha para o conteúdo e para o tipo)
+// Estritamente diferente (tipo e valor)
+console.log(one !== "1") // true (olha para o conteúdo e para o tipo) no caso conteúdo é igual, o tipo não, então é true porque é diferente
+
+console.log(one != two) // true
+console.log(one != 1) // false 
+console.log(one != "1") // false (olha para o conteúdo e não para o tipo)
+
+console.log(one > two) // false
+console.log(one < two) // false
