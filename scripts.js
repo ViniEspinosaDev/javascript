6abb4b88f06ce129d19f1db7a39f47ea52ea7206
ViniEspinosaDev <viniespinosa.developer@gmail.com>
2025-10-31 07:44:36 -0300

    Hoisting

--- scripts.js
+++ scripts.js
@@ -1,5 +1,21 @@
-const number = 33
-console.log(number)
+/* Hoisting (levantar ou içar) 
 
-number = 4 // Dá erro
-console.log(number)
+    se refere ao comportamento do interpretador de mover
+    as declarações de variáveis e funções para o topo do escopo em que foram definidas,
+    antes mesmo da execução do código.
+
+    esse comportamento possibilita usar uma variável ou função antes que ela esteja definida.
+    
+    se usar antes da variável ser declarada, vai ser undefined.
+
+    função também.
+
+    usar mais o let do que o var. 
+    var só quando for escopo global
+
+    hoisting só funciona com o var
+*/
+
+console.log(user)
+
+var user = "user"
