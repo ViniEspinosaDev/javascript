9453991e2301c3c13f4007be6023c823009b2e01
ViniEspinosaDev <viniespinosa.developer@gmail.com>
2025-10-31 09:19:59 -0300

    Falsy and truthy

--- scripts.js
+++ scripts.js
@@ -1,18 +1,22 @@
-let one = 1
-let two = 2
+console.log("### EXEMPLOS FALSY ###")
+console.log(false ? "TRUE": "FALSE") // false
+console.log(0 ? "TRUE": "FALSE") // false
+console.log(-0 ? "TRUE": "FALSE") // false
+console.log("" ? "TRUE": "FALSE") // false
+console.log(null ? "TRUE": "FALSE") // false
+console.log(NaN ? "TRUE": "FALSE") // false
+console.log(undefined ? "TRUE": "FALSE") // false
 
-console.log(one == two) // false
-console.log(one == 1) // true
-console.log(one == "1") // true (olha para o conteúdo e não para o tipo)
-
-// Estritamente igual (tipo e valor)
-console.log(one === "1") // false (olha para o conteúdo e para o tipo)
-// Estritamente diferente (tipo e valor)
-console.log(one !== "1") // true (olha para o conteúdo e para o tipo) no caso conteúdo é igual, o tipo não, então é true porque é diferente
-
-console.log(one != two) // true
-console.log(one != 1) // false 
-console.log(one != "1") // false (olha para o conteúdo e não para o tipo)
-
-console.log(one > two) // false
-console.log(one < two) // false
+console.log("### EXEMPLOS TRUTHY")
+console.log(true ? "TRUE": "FALSE") // true
+console.log(1 ? "TRUE": "FALSE") // true
+console.log("aa" ? "TRUE": "FALSE") // true
+console.log(" " ? "TRUE": "FALSE") // true
+console.log("false" ? "TRUE": "FALSE") // true
+console.log(-1 ? "TRUE": "FALSE") // true
+console.log(1221 ? "TRUE": "FALSE") // true
+console.log(-1221 ? "TRUE": "FALSE") // true
+console.log(-1.221 ? "TRUE": "FALSE") // true
+console.log({} ? "TRUE": "FALSE") // true
+console.log([] ? "TRUE": "FALSE") // true
+console.log(Infinity ? "TRUE": "FALSE") // true
