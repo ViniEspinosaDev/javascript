41b3a0742050ea370047c031ba324569ed9f43f2
ViniEspinosaDev <viniespinosa.developer@gmail.com>
2025-10-31 08:14:17 -0300

    conversão e coersão de tipos

--- scripts.js
+++ scripts.js
@@ -1,8 +1,25 @@
-// undefined (indefinido) - js atribui automaticamente para variável sem valor
-// null - não tem nada, colocado intencionalmente
+/* 
+    Conversão de tipos
+    - feito de forma consciente
+*/
 
-let emptiness
-console.log("O valor é: ", emptiness)
+console.log(typeof 9); // number
+console.log(typeof "9"); // string
+console.log(typeof Number("9")); // number
 
-let empty = null
-console.log("O valor é: ", empty)
+let age = 18;
+console.log(typeof age.toString()); // string
+console.log(typeof String(age)) // string
+
+let option = 1
+console.log(option)
+console.log(Boolean(option))
+console.log(typeof option)
+console.log(typeof Boolean(option))
+
+/* 
+    Coersão de tipo 
+    - acontece de forma automática (implicitamente)
+*/
+console.log("10" + 5) // 105
+console.log(typeof ("10" + 5)) // string
