069dd6fb46f92ebf732daa80ce1e8f12f4281faa
ViniEspinosaDev <viniespinosa.developer@gmail.com>
2025-10-31 12:03:35 -0300

    Arrow function

--- scripts.js
+++ scripts.js
@@ -1,15 +1,11 @@
-// Função anônima (função que não possui nome)
-const showMessage = function () {
-   return "Olá, Vinicius"
+// Arrow function
+const showMessage = () => {
+   console.log("Olá");
 };
 
-console.log(showMessage) // ƒ () { return "Olá, Vinicius" }
-console.log(showMessage()); // Olá, Vinicius
+const showMessage2 = (name, email) => {
+   console.log(email, name);
+};
 
-const executeSum = function(number1, number2) {
-    let result = number1 + number2
-    return result
-}
-
-console.log(executeSum) // ƒ (number1, number2) { let result = number1 + number2 return result }
-console.log(executeSum(1, 2)) // 3
+showMessage()
+showMessage2("vini", "vini@email.com")
