9a0badaa1dfdb535dd6a803206b2403f96a27f32
ViniEspinosaDev <viniespinosa.developer@gmail.com>
2025-10-31 08:05:46 -0300

    Template literals

--- scripts.js
+++ scripts.js
@@ -1,28 +1,12 @@
-// Tipos (dinâmico)
-/* */
+// Template literals (template string, interpolação de string)
+let username = "Vinicius"
+let email = "vinicius@email.com"
 
-/* Primitivos 
-string
-number
-boolean
-null
-undefined
-object
-array
-*/
+// Passar mais de um parâmetro
+console.log(username, email, "teste")
 
-// string
-let username = "Rodrigo"
-console.log(username)
-console.log(typeof username)
-console.log("Uma string com aspas duplas")
-console.log('Uma string com apóstrofo (aspas simples')
+// Concatenar texto
+console.log("Olá, " + username + ". Você conectou com o e-mail " + email)
 
-// Quando utilizar uma ou outra?
-console.log("Uma string com aspas duplas dentro de simples")
-console.log('Uma string com "aspas duplas" dentro de simples')
-console.log("Uma string com 'aspas simples' dentro de dupla")
-console.log(`Acendo grave, 
-    permite escrever multiplas linhas
-    respeita a formatação,
-    respeita a quebra`)
+// Template literals
+console.log(`Olá, ${username}. Você conectou com o e-mail ${email}`)
