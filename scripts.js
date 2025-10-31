22fe030ad96e8d4b0841af7435ee65d35b4b4507
ViniEspinosaDev <viniespinosa.developer@gmail.com>
2025-10-31 12:07:50 -0300

    callback function (putarias do javascript)

--- scripts.js
+++ scripts.js
@@ -1,11 +1,25 @@
-// Arrow function
-const showMessage = () => {
-   console.log("Olá");
-};
+// Callback function: Função passada para outra funcao como argumento
+function execute(taskName, callback) {
+   console.log("Executando a tarefa: ", taskName);
+   callback();
+}
 
-const showMessage2 = (name, email) => {
-   console.log(email, name);
-};
+function callback() {
+   console.log("Tarefa finalizada");
+}
 
-showMessage()
-showMessage2("vini", "vini@email.com")
+// Passando para a função
+execute("Donwload do arquivo", callback);
+
+// Criando a função no próprio parâmetro
+execute("Upload do arquivo...", function () {
+    console.log("Função de callback com uma função anônima.")
+});
+
+// Criando a função como arrow function no próprio parâmetro
+execute("Excluindo arquivo...", () => {
+    console.log("Arquivo excluído")
+})
+
+// Utilizar arrow function de forma encurtada (Quando é apenas uma linha)
+execute("Salvando arquivo...", () => console.log("Arquivo salvo!"))
