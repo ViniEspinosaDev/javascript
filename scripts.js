829f834b76f1bd11df9f0f310d72685091755b9e
ViniEspinosaDev <viniespinosa.developer@gmail.com>
2025-11-01 15:45:48 -0300

    Intl - internacionalização

--- scripts.js
+++ scripts.js
@@ -1,23 +1,13 @@
-// Values
-let amount = 12.5;
-let message = amount.toLocaleString("pt-BR", {
-   style: "currency",
-   currency: "BRL",
-});
-console.log(message);
+// Intl é a API de internacionalização do ECMAScript
+const currentLocale = Intl.DateTimeFormat().resolvedOptions();
+console.log(currentLocale);
 
-// Date
-let date = new Date("2025-10-02T21:33:33");
-let dateMessage = date.toLocaleString("pt-BR", {
-   dateStyle: "full",
-});
-console.log(dateMessage);
+// Exibe no formato de acordo com a localidade
+console.log(new Intl.DateTimeFormat("pt-BR").format(new Date()));
+console.log(new Intl.DateTimeFormat("en-US").format(new Date()));
 
-dateMessage = date.toLocaleString("pt-BR", {
-   day: "2-digit",
-   month: "2-digit",
-   year: "2-digit",
-   hour: "2-digit",
-   minute: "2-digit",
-});
-console.log(dateMessage);
+const date = new Date();
+// Diferença em minutos do timezone
+console.log(date.getTimezoneOffset());
+// Diferença em horas do timezone
+console.log(date.getTimezoneOffset() / 60);
