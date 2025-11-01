65b5917e9608d8608bd48cc9bd289b0c80dee319
ViniEspinosaDev <viniespinosa.developer@gmail.com>
2025-11-01 12:09:36 -0300

    Objects

--- scripts.js
+++ scripts.js
@@ -1,66 +1,137 @@
-const amount = document.getElementById("amount");
-const currency = document.getElementById("currency");
-const result = document.getElementById("result");
-const description = document.getElementById("description");
+// objeto vazio
+console.log("--- Objeto vazio---");
+const emptyObj = {};
 
-const usd = 5.38;
-const eur = 6.23;
-const gbp = 7.07;
+console.log(emptyObj);
+console.log(typeof emptyObj);
 
-function changeInfo() {
-   let symbol;
-   let amount;
-
-   switch (currency.value) {
-      case "USD":
-         symbol = "$";
-         amount = usd;
-         break;
-      case "EUR":
-         symbol = "€";
-         amount = eur;
-         break;
-      case "GBP":
-         symbol = "£";
-         amount = gbp;
-         break;
-   }
-
-   description.textContent = `${symbol} 1.00 - R$ ${amount.toFixed(2)}`;
-   console.log(description.textContent);
-}
-
-window.onload = () => {
-   console.log("oi");
-   changeInfo();
+// objeto com propriedades e métodos
+console.log("--- Objeto com propriedades e métodos ---");
+const user = {
+   email: "vini@email.com",
+   age: 25,
+   name: {
+      first: "Vinicius",
+      last: "Espinosa",
+   },
+   address: {
+      street: "Rua X",
+      number: 12,
+      city: "São Paulo",
+      postal_code: "180190-000",
+   },
+   messageArrowFunction: () => {
+      console.log(`Olá, ${user.name.first}`);
+      // Não funciona em arrow function
+      console.log(`usando o this fica assim, ${this.name.first}`);
+   },
+   messageAnonymousFunction: function () {
+      console.log(`Olá, ${user.name.first}`);
+      console.log(`usando o this fica assim, ${this.name.first}`);
+   },
 };
 
-amount.addEventListener("input", () => {
-   let newValue = amount.value.toString().replace(/[^0-9]/g, "");
-   amount.value = newValue;
-   changeInfo();
-   convert();
-});
+// Acessando propriedades e métodos usando notação de ponto
+console.log("--- Acessando propriedades e métodos notação de ponto ---");
+console.log(user.email);
+console.log(user.name.first);
+user.messageArrowFunction();
+user.messageAnonymousFunction();
 
-currency.addEventListener("change", () => {
-   changeInfo();
-   convert();
-});
+console.log("--- Acessando propriedades e métodos notação de colchetes ---");
+console.log("notação colchetes: ", user["email"]);
+console.log("notação colchetes: ", user["name"]["first"]);
+user["messageArrowFunction"]();
+user["messageAnonymousFunction"]();
 
-function convert() {
-   let calcResult;
+console.log("--- Atualizar objeto ---");
+const product = {
+   name: "Teclado",
+   quantity: 100,
+};
 
-   switch (currency.value) {
-      case "USD":
-         calcResult = amount.value / usd;
-         break;
-      case "EUR":
-         calcResult = amount.value / eur;
-         break;
-      case "GBP":
-         calcResult = amount.value / gbp;
-         break;
-   }
+console.log(product.name);
+console.log(product.quantity);
+product.quantity = 90;
+console.log(product.quantity);
+product["quantity"] = 50;
+console.log(product);
 
-   result.textContent = `R$${calcResult.toFixed(2)}`;
+/// Optional chaining
+console.log("--- Optional chaining ---");
+/* Encadeamento opcional. Se a propriedade ou função chamada é nullish (null or undefined), 
+   a expressão retorna undefined em vez de gerar um erro 
+*/
+
+const newUser = {
+   id: 1,
+   name: "Vini",
+   address: {
+      street: "Avenida Brasil",
+      city: "São Paulo",
+      geo: {
+         latitude: 23.123,
+         longitude: 88.3333,
+      },
+   },
+   message: function () {
+      console.log(`Olá, ${this.name}`);
+   },
+};
+
+console.log(newUser?.address?.street);
+newUser.message?.();
+
+/// Operador de coaslescência nula (??)
+console.log("--- Operador de coaslescência nula (??) ---");
+/* Operador lógico que retorna o seu operando do lado direito quando o seu operador do lado esquerdo é null ou undefined. 
+   caso contrário, ele retorna o seu operando do lado esquerdo
+*/
+let content = null;
+content = undefined;
+console.log(content ?? "Conteúdo é nulo");
+
+/// Função construtora
+console.log("--- Função construtora ---");
+function createProduct(name) {
+   const product = {};
+
+   product.name = name;
+   product.details = function () {
+      console.log(`O nome do produto é ${this.name}`);
+   };
+
+   return product;
 }
+
+// Nova copia na memória, criar instância
+const product1 = new createProduct("Sabonete");
+console.log(product1.name);
+product1.details();
+const product2 = new createProduct("Shampoo");
+console.log(product2.name);
+product2.details();
+
+// exemplos de funções construtoras do próprio javascript
+let myName = new String("Vini")
+console.log(myName)
+
+let price = "40.6".replace(".", "")
+console.log(price)
+
+let date = new Date("2024-11-02")
+console.log(date)
+
+// Dá pra fazer desse jeito, mas o javascript recomenda fazer classe
+function Person(name) {
+   this.name = name
+   this.message = function () {
+      console.log(`Ola, ${this.name}`)
+   }
+}
+
+const person1 = new Person("Rod")
+console.log(person1.name)
+
+const person2 = new Person("Vini")
+console.log(person2.name)
