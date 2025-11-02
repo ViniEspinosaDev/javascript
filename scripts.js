866b664e8f9832175defbda8156b5d07f9c52e87
ViniEspinosaDev <viniespinosa.developer@gmail.com>
2025-11-02 10:41:34 -0300

    find()

--- scripts.js
+++ scripts.js
@@ -1,10 +1,25 @@
 /*
-   findIndex() - retorna o índice no array do primeiro
-   elemento que satisfizer a condição. Caso contrário, 
-   retorna -1, indicando que nenhum elemento passou no teste
+   find() - retorna o valor do primeiro elemento
+   do array que satisfizer a condição. Caso contrário,
+   undefined é retornado
 */
 
 const numbers = [4, 6, 8, 12, 13];
 
-const index = numbers.findIndex((value) => value > 4);
-console.log(`Index: ${index} - Value: ${numbers[index]}`)
+// Retorna undefined
+const greaterThan1000 = numbers.find((value) => value > 1000);
+console.log(greaterThan1000);
+
+// Retorna primeiro elemento do array que condiz com a validação
+const greaterThan4 = numbers.find((value) => value > 4);
+console.log(greaterThan4);
+
+// Exemplo com objetos
+const fruits = [
+   { name: "Apple", quantity: 23 },
+   { name: "Banana", quantity: 13 },
+   { name: "Orange", quantity: 33 },
+];
+
+const result = fruits.find((fruit) => fruit.quantity > 20);
+console.log(result);
