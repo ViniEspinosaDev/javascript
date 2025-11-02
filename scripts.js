af389abc58537d5c8c52a7ca73f16da710d3cba7
ViniEspinosaDev <viniespinosa.developer@gmail.com>
2025-11-01 22:28:05 -0300

    conhecendo o spread (espalhamento)

--- scripts.js
+++ scripts.js
@@ -1,9 +1,32 @@
-// Rest params (...) permite representar um número
-// indefinido de argumentos como um array
+/*
+ spread (espalhar) permite que um objeto iterável, como uma 
+ expressão de array ou uma string seja expandido 
+ para ser usado onde zero ou mais argumentos
+*/
 
-function values(...args) {
-   console.log(...args);
-   console.log(args)
-}
+const numbers = [1, 2, 3];
+console.log(numbers);
 
-values(2, 1, 3, 4);
+console.log(...numbers);
+
+//
+const data = [
+   {
+      name: "João",
+      email: "joao@email.com",
+      avatar: "joao.png",
+   },
+   {
+      name: "Vini",
+      email: "vini@email.com",
+      avatar: "vini.png",
+   },
+   {
+      name: "Ana",
+      email: "ana@email.com",
+      avatar: "ana.png",
+   },
+];
+
+console.log(data)
+console.log(...data)
