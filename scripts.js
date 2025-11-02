95b9879e4935bc988fd602aaca3123eafd6c637c
ViniEspinosaDev <viniespinosa.developer@gmail.com>
2025-11-02 10:43:15 -0300

    every()

--- scripts.js
+++ scripts.js
@@ -1,25 +1,11 @@
 /*
-   find() - retorna o valor do primeiro elemento
-   do array que satisfizer a condição. Caso contrário,
-   undefined é retornado
+   every() - testa se todos os elementos
+   do array passam na condição e retorna
+   um valor boolean
 */
 
-const numbers = [4, 6, 8, 12, 13];
+const ages = [12, 18, 19, 23, 25];
 
 // Retorna undefined
-const greaterThan1000 = numbers.find((value) => value > 1000);
-console.log(greaterThan1000);
-
-// Retorna primeiro elemento do array que condiz com a validação
-const greaterThan4 = numbers.find((value) => value > 4);
-console.log(greaterThan4);
-
-// Exemplo com objetos
-const fruits = [
-   { name: "Apple", quantity: 23 },
-   { name: "Banana", quantity: 13 },
-   { name: "Orange", quantity: 33 },
-];
-
-const result = fruits.find((fruit) => fruit.quantity > 20);
+const result = ages.every((age) => age >= 18);
 console.log(result);
