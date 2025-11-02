dce77c36216717ce2c854dee1c98a5e072a1cdb7
ViniEspinosaDev <viniespinosa.developer@gmail.com>
2025-11-02 11:40:25 -0300

    Aplicando imutabilidade com o spread (espalhamento)

--- scripts.js
+++ scripts.js
@@ -1,26 +1,58 @@
 /*
-   reduce() - o método reduce é utilizado para
-   reduzir um array a um único valor
-
-   parâmetros:
-   - array original (values)
-   - acumulador (accumulator)
-   - valor da iteração (currentValue)
-   - valor inicial (0)
-   - index (index da iteração atual - opcional)
+   Imutabilidade - ao invés de criar uma referência
+   do objeto e ao alterar, mudar o original, o ideal
+   é copiar os dados para outro espaço da memória, 
+   para manter o original imutável
 */
 
-const values = [10, 20, 30, 40, 50];
+const address1 = {
+   street: "Av. Brasil",
+   number: 20,
+};
 
-const sum = values.reduce((accumulator, currentValue, index) => {
-   console.log("Acumulador: ", accumulator);
-   console.log("Current value: ", currentValue);
-   console.log("Index: ", index);
+// Isso não é uma cópia. É uma referência
+const address2 = address1;
+address2.number = 30;
 
-   console.log("Soma", accumulator + currentValue);
-   console.log("########");
+console.log(address1); // 30
+console.log(address2); // 30
 
-   return accumulator + currentValue;
-}, 0);
+// ------- Copiar 1
+const person = {
+   name: "Vinicius",
+   age: 25,
+};
 
-console.log("RESULTADO: ", sum);
+// Copia para outro endereço da memória
+const person2 = { ...person };
+person2.name = "Wilma";
+person2.age = 88;
+
+console.log(person); // Vinicius 25
+console.log(person2); // Wilma 88
+
+// ------- Copiar 2
+const product = {
+   description: "Teclado",
+   price: 80.5,
+};
+
+const productAfterDiscount = { ...product, price: 70 };
+
+console.log(product); // Teclado 80.5
+console.log(productAfterDiscount); // Teclado 70
+
+// ----- Exemplo com array
+// Não cria a cópia. É a mesma referência da memória
+const list1 = ["Apple", "Banana"];
+const list2 = list1;
+
+list2.push("Watermelon");
+
+// Cria cópia em outro endereço da memória. Tornando o original imutável
+const list3 = [...list1, "Strawberry"];
+list3.push("Orange", "Grape");
+
+console.log(list1); // apple, banana, watermelon
+console.log(list2); // apple, banana, watermelon
+console.log(list3); // apple, banana, watermelon, strawberry, orange, grape
