4bd8a464dc47ec51a6d2ba3d31c72ca659d27bd9
ViniEspinosaDev <viniespinosa.developer@gmail.com>
2025-11-06 00:12:13 -0300

    Event loop: ordem de execução das coisas

--- scripts.js
+++ scripts.js
@@ -1,38 +1,42 @@
 /*
- Promisse = Promessa, retorno de uma chamada assincrona,
- que pode ser resolvida ou rejeitada. Caso sucesso, resolve. Caso falha, rejeita
+javascript
+
+single thread
+no-blocking: não trava o contexto da execução
+asynchronous: por ser no-blocking precisa utilizar um paradigma assíncrono para lidar com a execução das coisas
+concurrent: as tarefas que executam assíncronamente concorrem uma com as outras pelo processamento
+
+o js possui um modelo de concorrência baseado em um event loop responsável por gerenciar a execução
+de código assíncrono e evento em um único thread, permitindo que o js seja não bloqueante
+
+call stack (pilha das funções em execução)
+callback queue (fila que armazena callbacks e eventos que aguardam para serem processados)
+Event loop: monitora a pilha de call stack e call back. Se houver, um callback é removido da fila e colocado na pilha de chamada
+de execução
+
+Fila de callback:
+- micro e macro tasks
+- microtask: alta prioridade (temporizadores e promises)
+- macrotask: menor prioridade, callback de eventos, settimeout e set interval
 */
 
-function asyncFunction() {
-   return new Promise((resolve, reject) => {
-      // Simula uma operação assíncrona
+// (1º) Executa o código de forma síncrona e o valor 1 é impresso imediatamente no console
+console.log(1)
 
-      setTimeout(() => {
-         const isSuccess = true;
+// (3º) Microtasks são executadas antes de temporazidaores e promisses
+queueMicrotask(() => {
+   console.log(2)
+})
 
-         if (isSuccess) {
-            resolve("A operação foi concluída com sucesso");
-         } else {
-            reject("Algo deu errado!");
-         }
-      }, 3000);
-   });
-}
+// (5º Macrotask que aguarda o evento de temporizador ser acionado)
+setTimeout(() => {
+   console.log(3)
+}, 1000)
 
-// const fetchArrowFunction = async () => {
-//    const response = await asyncFunction()
-//    console.log()
-// }
+// (2º) Execução síncrona.
+console.log(4)
 
-async function fetch() {
-   try {
-      const response = await asyncFunction();
-      console.log("Sucesso: ", response);
-   } catch (error) {
-      console.log("Erro: ", error);
-   } finally {
-      console.log("Fim do processo");
-   }
-}
-
-fetch();
+// (4º) Adiciona uma microtask (promise)
+Promise.resolve(true).then(() => {
+   console.log(5)
+})
