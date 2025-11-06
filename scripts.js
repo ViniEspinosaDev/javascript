e71d13e7b14c03aed9ebdbd9e1dcd92b04bcef4e
ViniEspinosaDev <viniespinosa.developer@gmail.com>
2025-11-05 23:42:54 -0300

    Set timeout

--- scripts.js
+++ scripts.js
@@ -1,25 +1,8 @@
-const book = {
-   title: "Objetos imutáveis",
-   category: "Javascript",
-   author: {
-      name: "Rodrigo",
-      email: "rodrigo@email.com",
-   },
-};
+/*
+setTimeout() = executa uma função
+após um intervalo de tempo especificado
+*/
 
-const updatedBook = {
-   ...book,
-   title: "Criando um front-end moderno com HTML",
-   category: "HTML",
-   type: "Programming",
-};
-
-// Original intacto
-console.log(book);
-
-// Modificado
-console.log(updatedBook);
-
-// Utilizando operador de desestruturação (rest operator) para remover propriedades
-const { type, ...bookWithoutCategory } = book;
-console.log(bookWithoutCategory);
+setTimeout(() => {
+   console.log("Olá, tudo bem?") // Mostra após 3 segundos
+}, 3000) // tempo em milissegundos
